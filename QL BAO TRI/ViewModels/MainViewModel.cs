fbf4e000feb7d450082aac0b9068d06ee014020a
truyen.lv <truyen.lv@THTC188>
2021-03-18 15:24:05 +0700

    tao giao dien

---
+++ QL BAO TRI/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Mvvm;
+using System;
+
+namespace QL_BAO_TRI.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        protected override void OnInitializeInDesignMode()
+        {
+            base.OnInitializeInDesignMode();
+            
+        }
+        protected override void OnInitializeInRuntime()
+        {
+            //tren giao dien người dung
+          
+        }
+        #region Khai báo Command
+        public DelegateCommand BtnTreoThe { get; private set; }
+        public DelegateCommand LoadForm_Command { get; private set; }
+        #endregion
+        public MainViewModel()
+        {
+            LoadForm_Command = new DelegateCommand(Load_form, true);
+        }
+
+        #region xu ly cac command
+        void Load_form()
+        {
+
+        }
+        #endregion
+    }
+}
